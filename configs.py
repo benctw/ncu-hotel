f887c808847c26e8baced14ddb4617184a9a6811
Jhen-Jie, Hsieh <a610161@gm.tnfsh.tn.edu.tw>
2022-04-27 23:48:42 +0800

    Add configs.py to set Line token & secret in code.

---
+++ configs.py
@@ -0,0 +1,6 @@
+# It's recommended to set the below variables in environment variables
+# instead of setting them here.
+# If you want to set them here, DON'T share the code publicly.
+
+LINE_CHANNEL_ACCESS_TOKEN = 'PASTE_YOUR_ACCESS_TOKEN_HERE'
+LINE_CHANNEL_SECRET = 'PASTE_YOUR_CHANNEL_SECRET_HERE'
--- line_chatbot_api.py
+++ line_chatbot_api.py
@@ -5,12 +5,12 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, 
+    MessageEvent,
     PostbackEvent,
-    TextMessage, 
-    TextSendMessage, 
-    ImageSendMessage, 
-    StickerSendMessage, 
+    TextMessage,
+    TextSendMessage,
+    ImageSendMessage,
+    StickerSendMessage,
     LocationSendMessage,
     TemplateSendMessage,
     ButtonsTemplate,
@@ -24,6 +24,10 @@ from linebot.models import (
     DatetimePickerAction,
     ConfirmTemplate
 )
+import os
+import configs
 
-line_bot_api = LineBotApi('f2V3d+/3MpYXb28+tUNODK+t4V+0ak9oe7GjE9KoI4Rl0cKGLbRG8tB/POdwy43kzn+7dxdYqW5RteF/cp7DobS3VW+qxAmriKoqkG2CfFje/7H+2UPQ3IMJ5uOGtkdKcsP3W2e3VMwgrCa5GNUAhwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('4134a0a296fcd96427f616f80eff50af')
+line_bot_api = LineBotApi(
+    os.getenv('LINE_CHANNEL_ACCESS_TOKEN', configs.LINE_CHANNEL_ACCESS_TOKEN))
+handler = WebhookHandler(
+    os.getenv('LINE_CHANNEL_SECRET', configs.LINE_CHANNEL_SECRET))
